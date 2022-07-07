42b1bb721b8765078fdd0053e682890c1c391e2b
tareita <tareitaa@outlook.com>
2022-07-07 20:24:04 +0100

    rest api get, post, delete endpoints made

--- index.js
+++ index.js
@@ -7,14 +7,33 @@ app.listen(3000, () => {
 
 app.use(express.json());
 
-const items = [];
+let posts = [];
+let count = 1;
 
-app.get("/items", (req, res) => {
-  return res.send(items);
+app.get("/posts", (req, res) => {
+  return res.send(posts);
 });
 
-app.post("/items", (req, res) => {
-  const text = req.body.text;
-  items.push(text);
-  return res.send("added item: " + text);
+app.get("/posts/:id", (req, res) => {
+  const id = req.params.id;
+  const post = posts.find((post) => post.id == id);
+  return res.send(post);
 });
+
+app.post("/posts", (req, res) => {
+  const title = req.body.title;
+  const content = req.body.content;
+  const author = req.body.author;
+  const post = { title: title, content: content, author: author, id: count };
+  count++;
+  posts.push(post);
+  return res.send("your new post: " + post.title + " " + post.content);
+});
+
+app.delete("/posts/:id", (req, res) => {
+  const id = req.params.id;
+  posts = posts.filter((post) => post.id != id);
+  return res.send(posts);
+});
+
+app.patch;
